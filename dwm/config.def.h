94a57e5955f9f487a7a8fc57708be691c9cb60fd
Rahmat Subekti <rahmatsubekti@live.com>
2020-07-18 15:35:12 +0700

    add surf and tabbed also some changes on dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -34,7 +34,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "⓵", "⓶", "⓷", "⓸", "⓹", "⓺", "⓻", "⓼", "⓽" };
 
 static const Rule rules[] = {
 	/* xprop(1):
